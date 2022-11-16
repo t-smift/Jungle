afbcb32ba8e0bdda37e08482b11c8f8a4e3542b2
Taylor Smith <taylor.l.smith07@gmail.com>
2022-11-16 13:27:16 -0800

    add admin dashboard with product and cat totals

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_username"], password: ENV["ADMIN_password"]
   def show
+    @categories_count = Category.count
+    @products_count = Product.count
   end
+ 
+
 end
