7d6f2fbb2f5541d9de8a939526499d4bdec578b1
Taylor Smith <taylor.l.smith07@gmail.com>
2022-11-17 12:16:23 -0800

    add session and user views, add routes to handle user auth

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,13 @@ Rails.application.routes.draw do
     post   :remove_item
   end
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  
   resources :orders, only: [:create, :show]
 
   namespace :admin do
@@ -18,6 +25,8 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
